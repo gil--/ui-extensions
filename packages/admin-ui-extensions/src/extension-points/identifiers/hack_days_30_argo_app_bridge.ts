594f58025a24c8fc000b4e8079ed2819668d5b12
Jeff Salerno <jeff.salerno@shopify.com>
2021-07-22 10:50:36 -0400

    added breadcrumbs interface

--- packages/admin-ui-extensions/src/extension-points/identifiers/hack_days_30_argo_app_bridge.ts
+++ packages/admin-ui-extensions/src/extension-points/identifiers/hack_days_30_argo_app_bridge.ts
@@ -2,7 +2,12 @@ import {RemoteRoot} from '@remote-ui/core';
 
 import {AllComponentsSchema} from '../../containers';
 
-import {RenderableExtensionCallback, StandardApi, ToastApi} from '../types';
+import {
+  ContainerAction,
+  RenderableExtensionCallback,
+  StandardApi,
+  ToastApi,
+} from '../types';
 
 // Add the unique extension point(s) as a union string
 // This example only contains a single extension point
@@ -11,10 +16,16 @@ export type HackDays30ArgoAppBridgeExtensionPoint =
   | 'Admin::App'
   | 'Admin::Product::MoreActions';
 
+export interface HackDays30ArgoAppBridgeContainerApi {
+  setBreadcrumbActions: (actions?: ContainerAction[]) => void;
+}
+
 // Update APIs for your needs
 // All Extension APIs should include the StandardApi by default
 export interface HackDays30ArgoAppBridgeExtensionApi {
-  'Admin::App': StandardApi<HackDays30ArgoAppBridgeExtensionPoint> & ToastApi;
+  'Admin::App': StandardApi<HackDays30ArgoAppBridgeExtensionPoint> &
+    ToastApi &
+    HackDays30ArgoAppBridgeContainerApi;
   'Admin::Product::MoreActions': StandardApi<
     HackDays30ArgoAppBridgeExtensionPoint
   > &
