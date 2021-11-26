f727b2580494533ead61b72d121f03e8e8cbe6b5
Phiroze Noble <phiroze.noble@shopify.com>
2021-11-26 15:05:59 -0500

    Ad Avatar Component

--- packages/admin-ui-extensions/src/components/index.ts
+++ packages/admin-ui-extensions/src/components/index.ts
@@ -48,3 +48,4 @@ export {Thumbnail} from './Thumbnail';
 export type {ThumbnailProps} from './Thumbnail';
 
 export type {DestructableAction, DisableableAction} from './types';
+export * from './Avatar';
