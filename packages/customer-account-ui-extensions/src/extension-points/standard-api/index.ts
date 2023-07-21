e9c84ed6b6c882ca578f7785e4daa7872a276f7a
Brian Shen <brian.shen@shopify.com>
2023-07-21 13:19:38 +0000

    remove customerApi from standardApi

--- packages/customer-account-ui-extensions/src/extension-points/standard-api/index.ts
+++ packages/customer-account-ui-extensions/src/extension-points/standard-api/index.ts
@@ -8,11 +8,6 @@ export type RendererVersion = 'unstable';
  * The following APIs are provided to all extension points.
  */
 export interface StandardApi {
-  customerApi: {
-    getEndpoint(version: 'unstable'): Promise<string>;
-    getAccessToken(): Promise<string>;
-  };
-
   /**
    * Meta information about the extension.
    */
